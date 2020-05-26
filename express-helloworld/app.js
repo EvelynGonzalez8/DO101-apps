10386932d4528fdcab26d0dbbcfd06d2703c6174
Evelyn Gonzalez <evelyn.gonzalez@compsesa.com.ec>
2020-05-26 16:36:10 -0500

    ok

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,21 @@
+var express = require('express');
 var response;
 
-app.get('/', function (req, res) {
-    response = 'This is version 2 of the app.' + '\n';
+app = express();
 
-    //send the response to the client
-    res.send(response);
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+app.get('/', function (req, res) {
+  response = 'This is version 2 of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
 
 });
+
